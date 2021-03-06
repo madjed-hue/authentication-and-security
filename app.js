f94ad9475e60cbec05374ad0b7d6aa27123a9297
unknown <kentounmax@gmail.com>
2021-03-06 11:05:17 +0100

    encryption with mongoose

--- app.js
+++ app.js
@@ -10,12 +10,18 @@ app.set('view engine', 'ejs');
 
 const mongoose = require('mongoose');
 
+const encrypt = require('mongoose-encryption');
+
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology:true});
 
-const userSchema ={
+const userSchema = new mongoose.Schema({
     email: String,
     password: String
-};
+});
+
+const secret = "Thisisourlittlesecret.";
+
+userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
 
 const User = new mongoose.model('User', userSchema);
 
